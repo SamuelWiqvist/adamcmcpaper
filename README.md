b20cfe7c7930f13c47604851e55a64232b8af8a4
SamuelWiqvist <swiqvist@gmail.com>
2018-02-07 15:45:03 +0100

    can now run the code on laptop

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Code for the paper *Accelerating delayed-acceptance Markov Chain Monte Carlo algorithms using Gaussian processes*
 
 This repository contains all code the draft paper *Accelerating delayed-acceptance Markov Chain Monte Carlo algorithms using Gaussian processes*.
+
+linked folders:
+
+* On desktiop: "C:\\Users\\samuel\\Dropbox\\Phd Education\\Projects\\project 1 accelerated DA and DWP SDE\\code"
+* On laptop: "C:\\Users\\samue\\OneDrive\\Documents\\GitHub\\adamcmcpaper"
--- Ricker model/rickermodel.jl
+++ Ricker model/rickermodel.jl
@@ -17,8 +17,8 @@ if Sys.CPU_CORES == 8
   include("C:\\Users\\samuel\\Dropbox\\Phd Education\\Projects\\project 1 accelerated DA and DWP SDE\\code\\gpmodel\\gp_model.jl")
   include("C:\\Users\\samuel\\Dropbox\\Phd Education\\Projects\\project 1 accelerated DA and DWP SDE\\code\\adaptive updating algorithms\\adaptiveupdate.jl")
 else
-  include("C:\\Users\\samue\\Dropbox\\Phd Education\\Projects\\project 1 accelerated DA and DWP SDE\\code\\gpmodel\\gp_model.jl")
-  include("C:\\Users\\samue\\Dropbox\\Phd Education\\Projects\\project 1 accelerated DA and DWP SDE\\code\\adaptive updating algorithms\\adaptiveupdate.jl")
+  include("C:\\Users\\samue\\OneDrive\\Documents\\GitHub\\adamcmcpaper\\gpmodel\\gp_model.jl")
+  include("C:\\Users\\samue\\OneDrive\\Documents\\GitHub\\adamcmcpaper\\adaptive updating algorithms\\adaptiveupdate.jl")
 end
 
 
