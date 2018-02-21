ece2a6e34e97b0edb9237bfa404ad8490a3a729b
SamuelWiqvist <swiqvist@gmail.com>
2018-02-21 20:49:25 +0100

    fix plotting script

--- DWPSDE model/Results/analyse_results.jl
+++ DWPSDE model/Results/analyse_results.jl
@@ -1,3 +1,4 @@
+# load packages
 
 using Plots
 using PyPlot
@@ -6,18 +7,18 @@ using KernelDensity
 using Distributions
 using DataFrames
 
+# set dir
 try
-  cd("DWPSDE model\\Results")
+    cd("DWPSDE model\\Results")
 catch
-  warn("Already in the Ricker model folder")
+    warn("Already in the Results folder for the DWP model.")
 end
-
 
 # load functions to compute posterior inference
 if Sys.CPU_CORES == 8
-  include("C:\\Users\\samuel\\Dropbox\\Phd Education\\Projects\\project 1 accelerated DA and DWP SDE\\code\\utilities\\posteriorinference.jl")
+    include("C:\\Users\\samuel\\Dropbox\\Phd Education\\Projects\\project 1 accelerated DA and DWP SDE\\code\\utilities\\posteriorinference.jl")
 else
-  include("C:\\Users\\samue\\OneDrive\\Documents\\GitHub\\adamcmcpaper\\utilities\\posteriorinference.jl")
+    include("C:\\Users\\samue\\OneDrive\\Documents\\GitHub\\adamcmcpaper\\utilities\\posteriorinference.jl")
 end
 
 # text and lable size
@@ -25,13 +26,11 @@ text_size = 15
 label_size = 15
 
 
-load_data_from_files = true # % load data from files or form some matlab workspace
-plot_prior_trace_plot = false
-ergp = ""# % set to _ergp to load ER-GP file  o.w. []
+load_data_from_files = true # load data from files or form some matlab workspace
+ergp = "" #  set to _ergp to load ER-GP file  o.w. []
 jobname = "test_new_calc_for_a" # set to jobname string
 
 if load_data_from_files
-
 
     data_res = convert(Array,readtable("output_res"*ergp*jobname*".csv"))
 
@@ -40,7 +39,7 @@ if load_data_from_files
     data_param = convert(Array,readtable("output_param"*ergp*jobname*".csv"))
 
     theta_true = data_param[1:N-2]
-    burn_in = data_param[N-2+1]
+    burn_in = Int64(data_param[N-2+1])
 
     data_prior_dist = convert(Array,readtable("output_prior_dist"*ergp*jobname*".csv"))
 
@@ -50,7 +49,6 @@ if load_data_from_files
     Z = convert(Array,readtable("data_used"*ergp*jobname*".csv"))
     Z = Z[:,1]
 
-
 else
 
     # this option should be used to load from stored .jld files
@@ -58,50 +56,48 @@ else
 end
 
 
-Theta = data_res[:,1:N-2]
+Theta = data_res[:,1:N-2]' # stor data in column-major order
 loglik = data_res[:,N-1]
 accept_vec = data_res[:,N]
 
 accept_rate = sum(accept_vec)/M
 nbr_acf_lags =  50
 
-acf = zeros(N-2,nbr_acf_lags+1)
+acf = ones(N-2,nbr_acf_lags+1)
+
+lower_q_int_limit = 2.5
+upper_q_int_limit = 97.5
 
 # use L"$\log r$"
 
 if N == 6
-    title_vec_log = [ L"$\log Kappa$"; L"$\log Gamma$"; L"$\log c$"; L"$\log d$"];
-    title_vec = [ 'Kappa'; 'Gamma'; 'c    '; 'd    '];
+    title_vec_log = [ L"$\log Kappa$"; L"$\log Gamma$"; L"$\log c$"; L"$\log d$"]
+    title_vec = [ "Kappa"; "Gamma"; "c"; "d"]
 elseif N == 4
-    title_vec_log = [ L"$\log c$"; L"$\log d$" ];
-    title_vec = [ 'c'; 'd' ];
+    title_vec_log = [ L"$\log c$"; L"$\log d$" ]
+    title_vec = [ "c"; "d" ];
 elseif N == 5
-    title_vec_log = [ L"$\log A$";L"$\log c$"; L"$\log d$" ];
-    title_vec = [ 'A';'c'; 'd' ];
+    title_vec_log = [ L"$\log A$";L"$\log c$"; L"$\log d$" ]
+    title_vec = [ "A";"c"; "d" ]
 elseif N == 8
-    title_vec_log = [ L"$\log A$"; L"$\log c$"; L"$\log d$"; L"$\log p_1$"; L"$\log p_2$"; L"$\log sigma$"];
-    title_vec = [  'A    '; 'c    '; 'd    '; 'p_1  '; 'p_1  '; 'sigma'];
+    title_vec_log = [ L"$\log A$"; L"$\log c$"; L"$\log d$"; L"$\log p_1$"; L"$\log p_2$"; L"$\log sigma$"]
+    title_vec = [  "A    "; "c    "; "d    "; "p_1  "; "p_1  "; "sigma"]
 elseif N == 7
     title_vec_log = [ L"$\log Kappa$"; L"$\log Gamma$"; L"$\log c$"; L"$\log d$"; L"$\log sigma$"];
-    title_vec = [ 'Kappa'; 'Gamma'; 'c    '; 'd    '; 'sigma'];
+    title_vec = [ "Kappa"; "Gamma"; "c"; "d"; "sigma"]
 elseif N == 9
-    title_vec_log = [ L"$\log Kappa$"; L"$\log Gamma$"; L"$\log c$"; L"$\log d$"; L"$\log p_1$"; L"$\log p_2$"; L"$\log sigma$"];
-    title_vec = [  'Kappa'; 'Gamma'; 'c    '; 'd    '; 'p_1  '; 'p_1  '; 'sigma'];
+    title_vec_log = [ L"$\log Kappa$"; L"$\log Gamma$"; L"$\log c$"; L"$\log d$"; L"$\log p_1$"; L"$\log p_2$"; L"$\log sigma$"]
+    title_vec = [  "Kappa"; "Gamma"; "c"; "d"; "p_1  "; "p_1  "; "sigma"]
 else
-    title_vec_log = [ L"$\log Kappa$"; L"$\log Gamma$"; L"$\log A$"; L"$\log c$"    '; L"$\log d$"    '; L"$\log g$"; L"$\log p_1$"; L"$\log p_2$"; L"$\log sigma$"];
-    title_vec = [ 'Kappa'; 'Gamma'; 'A    '; 'c    '; 'd    '; 'g    '; 'p_1  '; 'p_1  '; 'sigma'];
+    title_vec_log = [ L"$\log Kappa$"; L"$\log Gamma$"; L"$\log A$"; L"$\log c$"; L"$\log d$"; L"$\log g$"; L"$\log p_1$"; L"$\log p_2$"; L"$\log sigma$"]
+    title_vec = [ "Kappa"; "Gamma"; "A"; "c"; "d"; "g"; "p_1"; "p_1"; "sigma"]
 end
-
-for i = 1:N-2
-    [acf(i,:),lags_acf,bounds_acf] = autocor(Theta(burn_in:end,i),nbr_acf_lags);
-end
-
 
 acceptance_rate = sum(accept_vec[burn_in:end])/length(accept_vec[burn_in:end])
 
+# print info
 
 @printf "Accept rate: %.4f %% \n" acceptance_rate*100
-@printf "Nbr outside of prior: %d  \n" nbr_out_side_prior
 
 @printf "Posterior mean:\n"
 Base.showarray(STDOUT,mean(Theta[:,burn_in+1:end],2),false)
@@ -119,180 +115,144 @@ Base.showarray(STDOUT,calcquantileint(Theta[:,burn_in+1:end],lower_q_int_limit,u
 Base.showarray(STDOUT,RMSE(theta_true, Theta[:,burn_in+1:end]),false)
 @printf "\n"
 
-
-
 # plot trace plots
-Pyplot.figure()
-for i = 1:N-2
-  subplot(N-2,1,i)
-  plot(Theta(:,i))
-  hold on
-  plot(theta_true(i)*ones(1,length(Theta)), 'r--')
-  if plot_prior_trace_plot
-      plot(data_prior_dist(i,1)*ones(1,length(Theta)), 'g--')
-      plot(data_prior_dist(i,2)*ones(1,length(Theta)), 'g--')
-  end
-  ylabel(title_vec_log(i,:))
-end
-xlabel('Iteration')
 
-# plot trace plots on non-log scale
-Pyplot.figure()
+#on log-scale
+PyPlot.figure()
 for i = 1:N-2
-  subplot(N-2,1,i)
-  plot(exp(Theta(:,i)))
-  hold on
-  plot(exp(theta_true(i))*ones(1,length(Theta)), 'r--')
-  if plot_prior_trace_plot
-      plot(data_prior_dist(i,1)*ones(1,length(Theta)), 'g--')
-      plot(data_prior_dist(i,2)*ones(1,length(Theta)), 'g--')
-  end
-  ylabel(title_vec(i,:))
+    PyPlot.subplot(N-2,1,i)
+    PyPlot.plot(Theta[i,:])
+    PyPlot.plot(ones(size(Theta,2),1)*theta_true[i], "k")
+    PyPlot.ylabel(title_vec_log[i],fontsize=text_size)
 end
-xlabel('Iteration')
+PyPlot.xlabel("Iteration")
+
+# on non-log scale
+PyPlot.figure()
+for i = 1:N-2
+    PyPlot.subplot(N-2,1,i)
+    PyPlot.plot(exp.(Theta[i,:]))
+    PyPlot.plot(ones(size(Theta,2),1)*exp(theta_true[i]), "k")
+    PyPlot.ylabel(title_vec[i],fontsize=text_size)
+end
+PyPlot.xlabel("Iteration")
 
 # plot trace plots after burn in
-Pyplot.figure()
-x_axis = burn_in+1:size(Theta,1);
-for i = 1:N-2
-  subplot(N-2,1,i)
-  plot(x_axis, Theta(burn_in+1:end,i))
-  hold on
-  plot(x_axis, theta_true(i)*ones(1,length(Theta(burn_in+1:end,:))), 'r--')
-  if plot_prior_trace_plot
-      plot(x_axis, data_prior_dist(i,1)*ones(1,length(Theta(burn_in+1:end,:))), 'g--')
-      plot(x_axis, data_prior_dist(i,2)*ones(1,length(Theta(burn_in+1:end,:))), 'g--')
-  end
-  ylabel(title_vec_log(i,:))
 
+# on log-scale
+PyPlot.figure()
+x_axis = burn_in+1:size(Theta,2)
+for i = 1:N-2
+    PyPlot.subplot(N-2,1,i)
+    PyPlot.plot(x_axis, Theta[i,burn_in+1:end])
+    PyPlot.plot(x_axis, ones(length(x_axis),1)*theta_true[i], "k")
+    PyPlot.ylabel(title_vec_log[i])
 end
-xlabel('Iteration')
+PyPlot.xlabel("Iteration")
 
-
-Pyplot.figure()
+# on non-log scale
+PyPlot.figure()
+x_axis = burn_in+1:size(Theta,2)
 for i = 1:N-2
-  subplot(N-2,1,i)
-  plot(x_axis, exp(Theta(burn_in+1:end,i)))
-  hold on
-  plot(x_axis, exp(theta_true(i))*ones(1,length(Theta(burn_in+1:end,:))), 'r--')
-  if plot_prior_trace_plot
-      plot(x_axis, data_prior_dist(i,1)*ones(1,length(Theta(burn_in+1:end,:))), 'g--')
-      plot(x_axis, data_prior_dist(i,2)*ones(1,length(Theta(burn_in+1:end,:))), 'g--')
-  end
-  ylabel(title_vec(i,:))
+    PyPlot.subplot(N-2,1,i)
+    PyPlot.plot(x_axis, exp.(Theta[i,burn_in+1:end]))
+    PyPlot.plot(x_axis, ones(length(x_axis),1)*exp(theta_true[i]), "k")
+    PyPlot.ylabel(title_vec[i])
 end
-xlabel('Iteration')
+PyPlot.xlabel("Iteration")
 
-
-Pyplot.figure()
+# calc acf for each marginal chain
 for i = 1:N-2
-    subplot(N-2,1,i)
-    plot(lags_acf, acf(i,:))
-    title("Acf for " * title_vec_log(i,:))
-    xlabel('Lag')
+    acf[i,2:end] = autocor(Theta[i,burn_in:end],1:nbr_acf_lags)
 end
 
-
-Pyplot.figure()
+# plot acf
+PyPlot.figure()
 for i = 1:N-2
-  subplot(N-2,1,i)
-  [f,xi] = ksdensity(Theta(burn_in:end,i));
-  plot(xi,f)
+    PyPlot.subplot(N-2,1,i)
+    PyPlot.plot(0:nbr_acf_lags, acf[i,:])
+    PyPlot.ylabel(title_vec_log[i])
+end
+PyPlot.xlabel("Lag")
 
-  hold on
-  line([theta_true(i) theta_true(i)], get(gca, 'ylim'), 'color', 'r');
+# plot posterior
+PyPlot.figure()
+for i = 1:N-2
+    PyPlot.subplot(N-2,1,i)
+    h = kde(Theta[i,burn_in+1:end])
+    PyPlot.plot(h.x,h.density, "b")
 
-  if data_prior_dist_type == "Uniform"
-      if data_prior_dist(i,1) < 0
-         start_val = data_prior_dist(i,1)*1.05;
-      elseif data_prior_dist(i,1) > 0
-          start_val = data_prior_dist(i,1)*0.95;
-      else
-          start_val = -0.05;
-      end
-      if data_prior_dist(i,2) < 0
-          end_val = data_prior_dist(i,2)*.95;
-      elseif data_prior_dist(i,2) > 0
-          end_val = data_prior_dist(i,2)*1.05;
-      else
-          end_val = 0.05;
-      end
+    PyPlot.plot((theta_true[i], theta_true[i]), (0, maximum(h.density)), "k")
 
-      x_grid = start_val:0.001:end_val;
-      plot(x_grid, unifpdf(x_grid,data_prior_dist(i,1), data_prior_dist(i,2)), 'g')
-      axis([x_grid(1) x_grid(end) -inf inf])
-  elseif data_prior_dist_type = "Normal"
-      x_grid = (data_prior_dist(i,1)-4*data_prior_dist(i,2)):0.001:(data_prior_dist(i,1)+4*data_prior_dist(i,2));
-      plot(x_grid, normpdf(x_grid, data_prior_dist(i,1),data_prior_dist(i,2)),'g');
-      axis([x_grid(1) x_grid(end) -inf inf])
-  end
+    if data_prior_dist_type == "Uniform"
+        error("Uniform priors are not not implemented.")
+    elseif data_prior_dist_type == "Normal"
+        x_grid = (data_prior_dist[i,1]-4*data_prior_dist[i,2]):0.001:(data_prior_dist[i,1]+4*data_prior_dist[i,2])
+        PyPlot.plot(x_grid, pdf(Normal(data_prior_dist[i,1],data_prior_dist[i,2]), x_grid) ,"g")
+    end
 
-  title(title_vec_log(i,:))
+    PyPlot.ylabel(title_vec_log[i])
 end
 
-Pyplot.figure()
+# plot posterior, non-log scale
+PyPlot.figure()
 for i = 1:N-2
-  subplot(N-2,1,i)
-  [f,xi] = ksdensity(exp(Theta(burn_in:end,i)));
-  plot(xi,f)
-  hold on
-  line([exp(theta_true(i)) exp(theta_true(i))], get(gca, 'ylim'), 'color', 'r');
-  title(title_vec(i,:))
+    PyPlot.subplot(N-2,1,i)
+    h = kde(exp.(Theta[i,burn_in+1:end]))
+    PyPlot.plot(h.x,h.density, "b")
+    PyPlot.plot((exp(theta_true[i]), exp(theta_true[i])), (0, maximum(h.density)), "k")
+    PyPlot.ylabel(title_vec[i])
 end
 
-figure
-plot(loglik)
-ylabel(L"$\log-likelhood')
-xlabel('Iteration')
+# plot log-lik
+PyPlot.figure()
+PyPlot.plot(loglik)
+PyPlot.ylabel(L"$\log-likelhood")
+PyPlot.xlabel("Iteration")
 
 # plot acceptance rate for each K:th iteration
-k = 500;
+k = 500
 
-accept_vec_k = zeros(length(Theta)/k ,1);
-intervals = zeros(length(accept_vec_k),2);
+accept_vec_k = zeros(size(Theta,2)/k ,1)
+intervals = zeros(length(accept_vec_k),2)
 
-j = 1;
-for r = 2:length(Theta)
+j = 1
+for r = 2:size(Theta,2)
     if mod(r-1,k) == 0
-        accept_vec_k(j) = sum(accept_vec(r-k:r-1))/( r-1 - (r-k) );
-        intervals(j,:) = [r-k, r-1];
-        j = j +1;
+        accept_vec_k[j] = sum(accept_vec[r-k:r-1])/( r-1 - (r-k) ) * 100
+        intervals[j,:] = [r-k, r-1]
+        j = j +1
     end
 end
-accept_vec_k(end) = sum(accept_vec(length(accept_vec)-k+1:end))/( k );
 
-intervals(length(Theta)/k,:) = [length(accept_vec)-k+1, length(accept_vec)];
+accept_vec_k[end] = sum(accept_vec[(length(accept_vec)-k+1):end])/( k ) * 100
 
-intervals_vec = {'start'};
+intervals[Int(size(Theta,2)/k),:] = [length(accept_vec)-k+1, length(accept_vec)]
+
+# the "names" for the intervals are not used
+#=
+intervals_vec = {"start"};
 
 for j = 1:length(intervals)
-    intervals_vec(j) = {strcat(num2str(intervals(j,1)), '-', num2str(intervals(j,2)))}; % I should probably use vertical concatunate here!
+    intervals_vec(j) = {strcat(num2str(intervals(j,1)), "-", num2str(intervals(j,2)))}; % I should probably use vertical concatunate here!
 end
+=#
 
 PyPlot.figure()
-bar(k:k:length(Theta),accept_vec_k)
-%set(gca,'XTickLabel',intervals_vec)
-axis([0 length(Theta)+k 0 inf])
-ylabel('Acceptance rate')
-xlabel('Iteration')
+PyPlot.bar(1:length(accept_vec_k),accept_vec_k)
+PyPlot.ylabel("Acceptance rate")
+PyPlot.xlabel("Iteration")
 
 
 
-# plot simulated data
+# plot data
 PyPlot.figure()
-
-plot(1:length(Z),Z)
-axis([0 length(Z), -inf inf])
-xlabel('Index')
-%title('Data')
-set(gca,'FontSize',14)
+PyPlot.plot(1:length(Z),Z)
+PyPlot.xlabel("Index")
 
 PyPlot.figure()
-hist(Z,50)
-%title('Histogram')
-set(gca,'FontSize',14)
+PyPlot.plt[:hist](Z,50)
 
 # save results to jld file
-filename = 'ada_res';
 
-save(filename)
+# this feature is not needed...
