7dd737465ce0ed1dd1e7dac68af72b905c3634b2
Unknown <swiqvist@gmail.com>
2019-03-01 18:14:21 +0100

    updated calc ess

--- DWPSDE model/Results/calc_ESS.R
+++ DWPSDE model/Results/calc_ESS.R
@@ -2,81 +2,78 @@
 library(mcmcse)
 library(readr)
 
+# Real data
 
-# MCWM/PMCMC 
-output <- read_csv("gp_training_7_par_lunarc_simdata_4_coressimdata")
-View(output)
-
+# MCWM
+output <- read_csv("real data/output_resgp_training_7_par_lunarc_new_data_4_coresnew_data.csv")
+#View(output)
 X = (output)[10000:20000,1:7]
 t =  74795.716796139/2/60
 
-
 ess = ess(X)
 ess_min = min(ess)
-ess_min_per_sec = ess_min/t
-print(ess_min_per_sec)
-
-
-
+ess_min_per_sec_mcwm = ess_min/t
 
 # DA
-output <- read_csv("output_res_dagpest7_real_datada_gp_mcmc_biased_coin")
-View(output)
-
+output <- read_csv("real data/output_res_dagpest7new_datada_gp_mcmc.csv")
+#View(output)
 X = (output)[1:10000,1:7]
 t =  9474.373923259/60
 
-
 ess = ess(X)
 ess_min = min(ess)
-ess_min_per_sec = ess_min/t
-print(ess_min_per_sec)
-
-
+ess_min_per_sec_da = ess_min/t
 
 # ADA
-output <- read_csv("_dagpest7_real_dataada_gp_mcmc")
-View(output)
-
+output <- read_csv("real data/output_res_dagpest7new_dataada_gp_mcmc_dt.csv")
+#View(output)
 X = (output)[1:10000,1:7]
 t =  6053.148903795/60
 
+ess = ess(X)
+ess_min = min(ess)
+ess_min_per_sec_ada = ess_min/t
+
+# print
+print(cat("real data, MCWM:", ess_min_per_sec_mcwm))
+print(cat("real data, DA:", ess_min_per_sec_da))
+print(cat("real data, ADA:", ess_min_per_sec_ada))
+
+
+# sim data small problem 
+
+# MCWM
+output <- read_csv("sim data small problem/output_resgp_training_7_par_lunarc_simdata_4_coressimdata.csv")
+#View(output)
+X = (output)[10000:20000,1:7]
+t =  74795.716796139/2/60
 
 ess = ess(X)
 ess_min = min(ess)
-ess_min_per_sec = ess_min/t
-print(ess_min_per_sec)
+ess_min_per_sec_mcwm = ess_min/t
 
-
-# dagp  
-X = output_res_ergpest5_R5000_N25_25_direct_MH_desktop[11001:16000,1:5]
-t =  3154
-
-# load data for Ricker model 
-Theta_trans100000iter_2nd_run <- read_csv("C:/Users/samuel/Dropbox/Phd Education/LUNARC/Ricker model est 3 parameters v2/Results/Theta_trans100000iter_2nd_run.csv")
-View(Theta_trans100000iter_2nd_run)
-
-Theta_ergp_trans100000iter_2nd_run <- read_csv("C:/Users/samuel/Dropbox/Phd Education/LUNARC/Ricker model est 3 parameters v2/Results/Theta_ergp_trans100000iter_2nd_run.csv")
-View(Theta_ergp_trans100000iter_2nd_run)
-
-# mcwm 
-X = Theta_trans100000iter_2nd_run[2001:102000,1:3]
-t =  21.1*100
-
-
-# dagp  
-X = Theta_ergp_trans100000iter_2nd_run[4001:104000,1:3]
-t =  28.6*100
-
+# DA
+output <- read_csv("sim data small problem/output_res_dagpest7simdatada_gp_mcmc.csv")
+#View(output)
+X = (output)[1:10000,1:7]
+t =  9474.373923259/60
 
 ess = ess(X)
 ess_min = min(ess)
-ess_min_per_sec = ess_min/t
+ess_min_per_sec_da = ess_min/t
 
-# store value
-ess_min_per_sec_mcwm = ess_min_per_sec
-ess_min_per_sec_dagp = ess_min_per_sec
+# ADA
+output <- read_csv("sim data small problem/output_res_dagpest7simdataada_gp_mcmc_dt.csv")
+#View(output)
+X = (output)[1:10000,1:7]
+t =  6053.148903795/60
 
-# compute relative ESS_min/sec  
-ess_min_per_sec_mcwm/ess_min_per_sec_mcwm
-ess_min_per_sec_dagp/ess_min_per_sec_mcwm
+ess = ess(X)
+ess_min = min(ess)
+ess_min_per_sec_ada = ess_min/t
+
+# print
+print(cat("real data, MCWM:", ess_min_per_sec_mcwm))
+print(cat("real data, DA:", ess_min_per_sec_da))
+print(cat("real data, ADA:", ess_min_per_sec_ada))
+
